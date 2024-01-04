d3ecf00d52db52d9e678cac5ba20cf5b2328bae4
Enric Broto Hernandez <enrique7siete@gmail.com>
2024-01-04 18:03:32 +0100

    [add] header component + Navigation Links

--- moviehub_front_nextjs/src/app/home/layout.tsx
+++ moviehub_front_nextjs/src/app/home/layout.tsx
@@ -1,12 +1,15 @@
 
 import styles from './home.module.css'
+import Link from 'next/link'
 
 import { Inter } from 'next/font/google'
 import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
+
 import logo from '../../assets/movieShareLogo.png'
 
+import HeaderBar from '@/components/header/Header'
 
 
 export default function HomeLayout({
@@ -16,10 +19,14 @@ export default function HomeLayout({
 }) {
   return (
     <>
-      <header className={styles.header}>
-      <Image className={styles.logo} src={logo} alt="me" width="300" height="100" priority={true} />
+      {/* <header className={styles.header}>
+        <Link href="/home" >
+        <Image className={styles.logo} src={logo} alt="me" width="300" height="100" priority={true} />
 
-      </header>
+        </Link>
+
+      </header> */}
+      <HeaderBar />
       <main className={styles.main}>
         {children}
       </main>
---
+++ moviehub_front_nextjs/src/components/header/Header.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import logo from '../../assets/movieShareLogo.png'
+import './header.css'
+import { FaUser } from "react-icons/fa";
+import { RiLogoutCircleLine } from "react-icons/ri";
+import { BsExclamationCircle } from "react-icons/bs";
+
+type MenuItem = {
+  name: string,
+  icon: React.ReactNode,
+  href: string
+}
+const userId = 1
+
+const menuItems: MenuItem[] = [
+  {
+    name: "User",
+    icon: <FaUser />,
+    href: `/home/user/${userId}`
+  },
+  {
+    name: "Logout",
+    icon: <RiLogoutCircleLine />,
+    href: `/`
+  },
+  {
+    name: "About us",
+    icon: <BsExclamationCircle />,
+    href: `/home/AboutUs`
+  }
+]
+
+
+const HeaderBar = () => {
+
+
+  return (
+    <header className='header'>
+      <Link href="/home" >
+        <Image className="logo" src={logo} alt="me" width="300" height="100" priority={true} />
+      </Link>
+      <ul>
+        {menuItems.map(({ name, icon, href }) => (
+          <li>
+            <Link href={href}> {icon} {name} </Link>
+          </li>
+        ))}
+      </ul>
+    </header>
+  )
+}
+
+export default HeaderBar
